661706b406dd25dad69c24b6b408d94a8c15f86e
Harikrishnan G <harikrishnan.g@keyvalue.systems>
2024-05-21 10:40:23 +0530

    add chat history changes for internal policy chatbot

--- internal-policy-chatbot/use-case-2.py
+++ internal-policy-chatbot/use-case-2.py
@@ -1,58 +1,110 @@
-'''
+"""
 2. Use Case: Streamlining Internal Policy Inquiries with a Chat Application
 
 Scenario:  Employees frequently have questions about company policies and procedures. Traditionally, they might need to search through a company intranet, HR portal, or even email HR directly. This process can be time-consuming and inefficient, especially for simple questions.
 Solution: Integrate a knowledge base chatbot functionality within the company's existing chat application.
-'''
+"""
 
 # pip install langchain (version = 0.1.20)
 
-import streamlit as st
 import dotenv
-from langchain.chains.combine_documents.stuff import StuffDocumentsChain
-from langchain.chains.llm import LLMChain
-from langchain_core.prompts import PromptTemplate
-from langchain_openai import ChatOpenAI
+import json
+import streamlit as st
+from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
+from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_community.document_loaders import PyPDFDirectoryLoader
-from langchain.memory import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_openai import ChatOpenAI
 
 dotenv.load_dotenv()
-documents_directory = 'docs/'
-internal_policy_chat_message_history = ChatMessageHistory()
+documents_directory = "docs/"
 
 st.title("Internal Policy Chatbot")
 
+store = {}
+
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
 if "messages" not in st.session_state:
-  st.session_state.messages = []
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-def generate_response(input_text):
-  loader = PyPDFDirectoryLoader(documents_directory, extract_images=True)
-  pages = loader.load_and_split()
 
-  prompt_template = input_text + " Take into context the documents which are conference policy document and holiday policy document, content: {text}"
-  print(prompt_template)
-  prompt = PromptTemplate.from_template(prompt_template)
-  chat = ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo-1106")
-  llm_chain = LLMChain(llm=chat, prompt=prompt)
-  stuff_chain = StuffDocumentsChain(llm_chain=llm_chain, document_variable_name="text")
-  llm_output = stuff_chain.invoke(pages)
-  return llm_output['output_text']
+def generate_response(input: str, config: dict):
+    loader = PyPDFDirectoryLoader(documents_directory, extract_images=True)
+    pages = loader.load_and_split()
+    system_message = """
+    Answer the user's questions based on the below context. 
+    If the context doesn't contain any relevant information to the question, just say "I don't know":
+
+    <context>
+    {context}
+    </context>
+    """
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_message,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+            HumanMessage(content=input),
+        ]
+    )
+    chat = ChatOpenAI(temperature=0.2, model_name="gpt-3.5-turbo-1106")
+    stuff_chain = create_stuff_documents_chain(chat, prompt)
+    with_message_history = RunnableWithMessageHistory(
+        stuff_chain,
+        get_session_history,
+        input_messages_key="messages",
+    )
+    response = with_message_history.invoke(
+        {
+            "messages": get_session_history(
+                config["configurable"]["session_id"]
+            ).messages,
+            "context": pages,
+        },
+        config=config,
+    )
+    print(response)
+    return response
+
 
 if prompt := st.chat_input("Say something"):
+    store_file = open("store.json", "r")
+    try:
+        chat_history = json.loads(store_file.read())
+    except json.decoder.JSONDecodeError:
+        chat_history = {}
+    config = {"configurable": {"session_id": "abc123"}}
+    for session_id, messages in chat_history.items():
+        store[session_id] = ChatMessageHistory()
+        for message in messages:
+            if message["role"] == "assistant":
+              store[session_id].add_message(AIMessage(content=message["content"]))
+            elif message["role"] == "user":
+              store[session_id].add_message(HumanMessage(content=message["content"]))
+    store_file.close()
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
 
     with st.chat_message("assistant"):
-        response = generate_response(prompt)
+        response = generate_response(prompt, config)
         st.markdown(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
-
-
-
-
-
+    store_file = open("store.json", "w")
+    store_file.write(json.dumps({"abc123": st.session_state.messages}))
+    store_file.close()
